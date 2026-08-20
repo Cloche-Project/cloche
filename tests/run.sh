fc1117f8e2bccdf78f0fe958c3689142fa436f71
Augusto Marques <augustolfmarques@gmail.com>
2026-08-19 21:09:20 -0300

    fix(tests): silence shellcheck false-positive on unused vars in run.sh
    IMAGE, BUILD_METHOD, and RECIPE are consumed by test-lib.sh, which lives
    in the separate cloche-utils repo. Codacy analyzes this repo in
    isolation and can't see that usage, so it flags the assignments as
    unused (SC2034).

--- tests/run.sh
+++ tests/run.sh
@@ -8,8 +8,11 @@ UTILS_DIR="${CLOCHE_UTILS_DIR:-../cloche-utils}"
 # shellcheck source=../../cloche-utils/testing/test-lib.sh
 source "$UTILS_DIR/testing/test-lib.sh"
 
+# shellcheck disable=SC2034 # used by test-lib.sh (cloche-utils, a separate repo Codacy can't see)
 IMAGE="${1:-cloche-test}"
+# shellcheck disable=SC2034 # used by test-lib.sh (cloche-utils, a separate repo Codacy can't see)
 BUILD_METHOD="bluebuild"
+# shellcheck disable=SC2034 # used by test-lib.sh (cloche-utils, a separate repo Codacy can't see)
 RECIPE="./recipes/cloche.yml"
 
 test_rpm_ostree_status() {
